3d17eae46d2fe1bcdd2d05814c85771197fadf0e
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-02-24 17:18:33 +0800

    debugging-3

--- src/app/task/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className="flex min-h-screen bg-gray-100">
-        {children}
-      </body>
-    </html>
-  );
-}
